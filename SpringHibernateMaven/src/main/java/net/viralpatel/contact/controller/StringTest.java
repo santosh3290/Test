c1d2c443462b17a09ab2f395f9d5cffb5889fa24
Santosh <Santosh@Santosh-PC>
2014-11-24 23:20:46 +0530

    sdas

--- SpringHibernateMaven/src/main/java/net/viralpatel/contact/controller/StringTest.java
+++ SpringHibernateMaven/src/main/java/net/viralpatel/contact/controller/StringTest.java
@@ -8,6 +8,17 @@ public class StringTest {
 		
 		String str1="monika";
 		
+		System.out.println(str.equals(str1));
+		System.out.println(str==str1);
+		
+		
+		String str2=new String("monika");
+		
+		String str3=new String("monika");
+		
+		System.out.println(str2.equals(str3));
+		System.out.println(str2==str3);
+		
 		
 	}
 }
